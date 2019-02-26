a043cf9baa7072e337f1d54689f3d65fa7ffcd84
Oskar van Velden <oskar@rakso.nl>
2019-02-26 12:18:49 +0100

    Refactor wave mechanism

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -5,47 +5,36 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] List<WaveConfiguration> waveConfigurations;
+    [SerializeField] bool looping = true;
 
-    WaveConfiguration currentWave;
-    int currentWaveIndex = 0;
-    Coroutine currentWaveCoroutine;
-
-    void Start() {
-        StartNextWave();
+    IEnumerator Start() {
+        do {
+            yield return StartCoroutine(SpawnAllWaves());
+        } while (looping);
     }
 
-    private void StartNextWave() {
-        if (currentWaveCoroutine != null) {
-            StopCoroutine(currentWaveCoroutine);
+    private IEnumerator SpawnAllWaves() {
+        for( int waveIndex = 0; waveIndex < waveConfigurations.Count; waveIndex++ ) {
+            var currentWave = waveConfigurations[waveIndex];
+
+            yield return StartCoroutine(SpawnAllEnemiesInWave(currentWave));
         }
-
-        currentWave = waveConfigurations[currentWaveIndex];
-
-        currentWaveCoroutine = StartCoroutine(MakeWaveCoroutine());
     }
 
-    private IEnumerator MakeWaveCoroutine() {
-        for (int enemyCount = 0; enemyCount < currentWave.GetNumberOfEnemiesToSpawn(); enemyCount++) {
-            SpawnEnemy();
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfiguration waveConfiguration) {
+        for (int enemyCount = 0; enemyCount < waveConfiguration.GetNumberOfEnemiesToSpawn(); enemyCount++) {
+            SpawnEnemy(waveConfiguration);
             
-            yield return new WaitForSeconds(currentWave.GetTimeBetweenWaves() + Random.Range(currentWave.GetRandomFactor() * -1, currentWave.GetRandomFactor()));
+            yield return new WaitForSeconds(waveConfiguration.GetTimeBetweenWaves() + Random.Range(waveConfiguration.GetRandomFactor() * -1, waveConfiguration.GetRandomFactor()));
         }
-
-        currentWaveIndex++;
-
-        if (currentWaveIndex >= waveConfigurations.Count) {
-            currentWaveIndex = 0;
-        }
-
-        StartNextWave();
     }
 
-    private void SpawnEnemy() {
-        List<Transform> waypoints = currentWave.GetWaypoints();
+    private void SpawnEnemy(WaveConfiguration waveConfiguration) {
+        List<Transform> waypoints = waveConfiguration.GetWaypoints();
 
-        GameObject enemy = Instantiate(currentWave.GetEnemyPrefab(), waypoints[1].position, Quaternion.identity);
+        GameObject enemy = Instantiate(waveConfiguration.GetEnemyPrefab(), waypoints[1].position, Quaternion.identity);
         EnemyPathing enemyController = enemy.GetComponent<EnemyPathing>();
         enemyController.SetWaypoints(waypoints);
-        enemyController.SetMovementSpeed(currentWave.GetMovementSpeed());
+        enemyController.SetMovementSpeed(waveConfiguration.GetMovementSpeed());
     }
 }
