f7a2b31f4c52f6a0e1ed980b0dd30751210cc692
Oskar van Velden <oskar@rakso.nl>
2019-03-06 08:29:00 +0100

    Add explosion prefab

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] float healthPoints = 500f;
+    [SerializeField] GameObject explosionPrefab;
 
     public void TakeDamage(float damageTaken) {
         healthPoints -= damageTaken;
 
         if (healthPoints <= 0) {
+            if (explosionPrefab != null) {
+                Instantiate(explosionPrefab, new Vector2(transform.position.x, transform.position.y), Quaternion.identity);
+            }
+
             Destroy(gameObject);
         }
     }
