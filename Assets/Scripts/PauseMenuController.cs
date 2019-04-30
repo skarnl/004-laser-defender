523f301136a7512386195aa154ccdade527c9785
Oskar van Velden <oskar@rakso.nl>
2019-04-30 08:49:04 +0200

    Add pause menu
    This closes #2

---
+++ Assets/Scripts/PauseMenuController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject pauseMenu;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            pauseMenu.SetActive(!pauseMenu.active);
+        }
+    }
+}
